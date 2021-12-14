5206b15ce65a13914eec92c6fb50aa7c10edfe56
GarmaNazareno <garmatorresnazareno@gmail.com>
2021-12-14 03:07:52 -0300

    design finished

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var nav = document.querySelector('nav'); 
+
+window.addEventListener('scroll', function(event) { 
+    event.preventDefault();
+
+    if (window.scrollY <= 260) { 
+        nav.style.backgroundColor = '#000'; 
+        
+    } else {
+        nav.style.backgroundColor = 'purple';
+        
+       
+    }
+
+});
+
+
+document.addEventListener("click",function (e){
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+      myModal.show();
+  }
+})
+
